6a54b59ffc407793294672648695422be2ba5fba
Reuben Brasher <rkbrasher@gmail.com>
2026-09-12 10:15:45 -0700

    Modernize tooling, packaging and development conventions (#11)
    * Modernize tooling, packaging and development conventions
    
    * Update locked dependencies to clear security advisories

--- template/directories.py
+++ template/directories.py
@@ -1,24 +1,27 @@
-import os
+from pathlib import Path
 
 
 def qualifyname(directoryname, filename=None):
+    directory = Path(directoryname)
     if filename is None:
-        return directoryname
-    return os.path.join(directoryname, filename)
+        return directory
+    return directory / filename
 
 
 def code(filename=None):
-    codepath = os.path.dirname(__file__)
-    return qualifyname(codepath, filename)
+    return qualifyname(Path(__file__).parent.resolve(), filename)
 
 
 def base(filename=None):
-    basepath = os.path.abspath(code(".."))
-    return qualifyname(basepath, filename)
+    return qualifyname(code().parent, filename)
 
 
 def data(filename=None):
     return qualifyname(base("data"), filename)
+
+
+def package_data(filename=None):
+    return qualifyname(code("data"), filename)
 
 
 def tests(filename=None):
